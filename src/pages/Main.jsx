c20835a462a1983c692da23e0d0e39ecd6cef959
snowflaver <sirayukiyui@gmail.com>
2023-05-11 02:28:39 +0900

    메인 사진 베너 수정(슬라이드 추가)

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,17 +6,32 @@ import { useAppLogic } from '../components/hooks/useAppLogic';
 import { UploadModal } from '../modals/upload.jsx';
 import BestPicArea from '../components/BestPicArea';
 import Cookies from 'js-cookie';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
 const Main = () => {
   const { handleUploadClick, uploadModalOpen, setUploadModalOpen, handleUpload } = useAppLogic();
   const cookie = Cookies.get('token');
+
+  const images = [
+    'https://r1.community.samsung.com/t5/image/serverpage/image-id/1558989iD806B27D72FD5AC9/image-dimensions/2000?v=v2&px=-1',
+    'https://marketplace.canva.com/EAD2xI0GoM0/1/0/1600w/canva-%ED%95%98%EB%8A%98-%EC%95%BC%EC%99%B8-%EC%9E%90%EC%97%B0-%EC%98%81%EA%B0%90-%EC%9D%B8%EC%9A%A9%EB%AC%B8-%EB%8D%B0%EC%8A%A4%ED%81%AC%ED%86%B1-%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4-rssvAb9JL4I.jpg',
+  ]
+
   return (
     <Layout>
       <MainPic>
-        <img
+        <Carousel autoPlay infiniteLoop interval={6000}>
+          {images.map((imageUrl, index) => (
+            <div key={index}>
+              <StyledImg src={imageUrl} alt={`Slide ${index}`} />
+            </div>
+          ))}
+        </Carousel>
+        {/* <img
           src='https://marketplace.canva.com/EAD2xI0GoM0/1/0/1600w/canva-%ED%95%98%EB%8A%98-%EC%95%BC%EC%99%B8-%EC%9E%90%EC%97%B0-%EC%98%81%EA%B0%90-%EC%9D%B8%EC%9A%A9%EB%AC%B8-%EB%8D%B0%EC%8A%A4%ED%81%AC%ED%86%B1-%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4-rssvAb9JL4I.jpg'
           alt='메인이미지'
-        />
+        /> */}
       </MainPic>
 
       <SharButtonContainer>
@@ -45,6 +60,12 @@ const Main = () => {
     </Layout>
   );
 };
+
+const StyledImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
 const MainPic = styled.div`
   width: 100%;
