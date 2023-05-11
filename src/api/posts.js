02e6f1ab99027e72920079a04face0fa5af8f135
Ji-Tae <117277785+Ji-Tae@users.noreply.github.com>
2023-05-11 23:58:15 +0900

    콘솔 확인

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,11 +3,13 @@ import Cookies from 'js-cookie';
 // 최신 사진 겟 요청
 const getNewPictures = async () => {
   const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/posts/newposts`, { withCredentials: true });
+  console.log(`뉴 픽쳐 ${response.data}`);
   return response.data;
 };
 //베스트 사진 겟 요청
 const getBestPictures = async () => {
   const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/posts/bestposts`, { withCredentials: true });
+  console.log(`베스트 픽쳐 ${response.data}`);
   return response.data;
 };
 
